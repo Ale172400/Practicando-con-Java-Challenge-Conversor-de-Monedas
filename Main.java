6f1b5a9f48b1b242dedb30878995c463715be52e
Ale172400 <sanchezsantillanl@gmail.com>
2026-03-10 20:32:23 -0600

    Add Main class with menu initialization

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.conversor;
+
+import com.conversor.menu.Menu;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+        menu.iniciar();
+
+    }
+
+}

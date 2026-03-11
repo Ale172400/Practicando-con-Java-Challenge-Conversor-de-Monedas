c004b1ed0717233acc784e6464f82903e89fd01d
Ale172400 <sanchezsantillanl@gmail.com>
2026-03-10 20:34:50 -0600

    Add ExchangeResponse class with conversion rate field

---
+++ ExchangeResponse.java
@@ -0,0 +1,11 @@
+package com.conversor.model;
+
+public class ExchangeResponse {
+
+    private double conversion_rate;
+
+    public double getConversion_rate() {
+        return conversion_rate;
+    }
+
+}
